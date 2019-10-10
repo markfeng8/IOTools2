8b288a3625f2518e7c5fbc7c1f1de159a5813230
Your Name <you@example.com>
2019-10-10 12:54:18 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/io/mylibrary/FctUtils.java
@@ -0,0 +1,14 @@
+package com.io.mylibrary;
+
+/**
+ * Created by fct on 2018/7/12.
+ */
+
+public class FctUtils {
+
+    public static void getString() {
+    }
+
+    public static void setString() {
+    }
+}
